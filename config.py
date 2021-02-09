e3e698df506b5f8b05056b10cd3bb14fa1f2b279
thesmith4734 <thehellhound4734@aim.com>
2021-02-08 23:24:22 -0800

    Add to part 11

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['youremial@example.com']
+    ADMINS = ['smith.greg473444@gmail.com']
     POSTS_PER_PAGE = 25
