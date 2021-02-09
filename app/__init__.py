93dcf91ac60664771b4e13425bfc4309af4eeebf
thesmith4734 <thehellhound4734@aim.com>
2021-02-08 23:42:43 -0800

    part 12: Timestamp changes

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -17,6 +18,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
 mail = Mail(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
